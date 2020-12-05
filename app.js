be74f06ef32174ef125a3c0d7a32d3f2849fa49b
shivom <singhshivom230@gmail.com>
2020-12-05 23:48:10 +0530

    added server url

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ var txtButton = document.querySelector("#txt-button");
 var txtArea = document.querySelector(".textarea");
 var txtOutput = document.querySelector("#output-area");
 
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+function colUrl(text){
+    return serverUrl + "?text=" + text
+}
 
 function clickHandler() {
-    txtOutput.innerText = "allaallalla" + txtArea.value;
+    fetch(colUrl(txtArea.value)) //input
+    .then(respone => respone.json()) //processing
+    .then(json => {
+
+    var translatedText = json.contents.translated
+
+    txtOutput.innerText= translatedText  //ouptut
+    })
 }
 
 
