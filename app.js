7e94ad2f3324a507d3cf1644cb76c2ca953ba374
shivom <singhshivom230@gmail.com>
2020-12-05 21:21:25 +0530

    minor updates

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ var txtButton = document.querySelector("#txt-button");
 var txtArea = document.querySelector(".textarea");
 var txtOutput = document.querySelector("#output-area");
 
-txtOutput.innerText = "i am ironman"
+
 
 function clickHandler() {
-    console.log("clicked")
-    console.log("input", txtArea.value)
-   
+    txtOutput.innerText = "allaallalla" + txtArea.value;
 }
 
 
