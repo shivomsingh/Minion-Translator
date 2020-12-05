96bd0ebff5d7e7aa4d0058311ddede49728596fd
shivom <singhshivom230@gmail.com>
2020-12-05 23:56:25 +0530

    funtranslation api

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var txtButton = document.querySelector("#txt-button");
 var txtArea = document.querySelector(".textarea");
 var txtOutput = document.querySelector("#output-area");
 
-var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
 function colUrl(text){
     return serverUrl + "?text=" + text
