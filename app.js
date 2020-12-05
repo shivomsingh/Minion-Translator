12ddc801b452d2f4339c8d4436c381d76673e174
shivom <singhshivom230@gmail.com>
2020-12-05 17:07:15 +0530

    added alert and prompt in js file

---
+++ app.js
@@ -0,0 +1,6 @@
+// var txtButton = document.querySelector("#txt-button");
+// console.log(txtButton);
+// console.log("hi")
+alert("this is working")
+var userName = prompt("what is ur name?")
+alert("hi " + userName)
